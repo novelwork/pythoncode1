4841d423f2d4ad44053f497b96e46a84509fbd4d
novelmisatake <fteguh088@gmail.com>
2023-09-10 14:14:51 +0800

    my first project!

---
+++ joke_percabangan_sekuensial.py
@@ -0,0 +1,18 @@
+print('mulai')
+print('ibu memberi perintah "beli 1 botol susu jika ada telur, beli 1 botol susu dan 6 biji telur"')
+print('anak menjawab "ok"')
+print('anak pergi ke toko')
+print('anak sampai di toko')
+
+jumlah_botol_susu = 122
+if jumlah_botol_susu > 0:
+    print('anak membeli susu')
+else:
+    print('anak membeli susu')
+
+jumlah_telur = 7
+if jumlah_telur >6:
+    print('dan anak membeli telur 6 butir')
+else:
+    print('anak tidak membeli telur')
+print('anak pulang dan menyerahkan belanjaan kepada ibu')
