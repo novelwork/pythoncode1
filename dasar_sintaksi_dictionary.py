d7c87907ff9c781ae82e320e9c40ec17f6e4a104
novelmisatake <fteguh088@gmail.com>
2023-09-15 15:44:39 +0800

    pembaruan dari while sebelumnya

---
+++ dasar_sintaksi_dictionary.py
@@ -0,0 +1,11 @@
+users = {
+    'id' : 1,
+    'name': 'leanne graham',
+    'username': 'bret',
+    'email' : 'sincere@april.biz',
+}
+print(users)
+print(users['id'])
+print(users['name'])
+print(users['username'])
+print(users['email'])
