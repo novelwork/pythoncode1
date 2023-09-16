cb1125c6cf8c79765c53e94ef63d8ba390bb926f
novelmisatake <fteguh088@gmail.com>
2023-09-16 15:37:27 +0800

    pembaruan dari while sebelumnya

--- dasar_sintaksi_dictionary.py
+++ dasar_sintaksi_dictionary.py
@@ -3,9 +3,36 @@ users = {
     'name': 'leanne graham',
     'username': 'bret',
     'email' : 'sincere@april.biz',
+    'address': {
+        'street': 'kulas light',
+        'suite': 'apt.556',
+        'city': '92998-3874',
+        'geo': {
+            'lat': '-37.3159',
+            'lng':'81.1496'
+        }
+    }
 }
 print(users)
 print(users['id'])
 print(users['name'])
 print(users['username'])
 print(users['email'])
+print(users['address'])
+print(users['address']['street'])
+print(users['address']['geo'])
+print(users['address']['geo'])
+print(users['address']['geo']['lat'])
+print(users['address']['geo']['lng'])
+
+print(users)
+print(type(users))
+print('\n ubah dict ke json')
+import json
+result = json.dumps(users)
+print(type(result))
+print(result)
+
+with open('result,json', 'w') as file:
+    json.dump(users, file)
+
